b2f9a6f57ea0d6c64451145a344466dec4339ee4
Leon <OfficialMuffin@protonmail.com>
2018-12-20 21:44:11 +0000

    Initial commit

---
+++ app/src/main/java/com/firstapp/admin/myfirstandroidapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.firstapp.admin.myfirstandroidapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton = findViewById<Button>(R.id.rollButton)
+        val diceTextView = findViewById<TextView>(R.id.diceTextView)
+        val seekBar = findViewById<SeekBar>(R.id.seekBar)
+
+        rollButton.setOnClickListener{
+            val rand = Random().nextInt(seekBar.progress) + 1
+            diceTextView.text = rand.toString()
+        }
+    }
+}
